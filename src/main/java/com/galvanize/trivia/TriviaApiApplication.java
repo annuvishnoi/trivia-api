4750fade5c87d63242034bf1f77c0fb2712946cb
Anurag Vishnoi <annu.vishnoi@gmail.com>
2021-02-02 19:32:28 -0500

    intial trivia project setup

---
+++ src/main/java/com/galvanize/trivia/TriviaApiApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.trivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/trivia/TriviaApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.galvanize.trivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TriviaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
